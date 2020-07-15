9921af66c85d7cf3bf5c3f43abcadaca638f984b
abdullahi <h.abdullahi1995@gmail.com>
2020-07-15 14:05:38 +0300

    first commit

---
+++ database/seeds/TodosSeeder.php
@@ -0,0 +1,17 @@
+<?php
+use App\Todo;
+
+use Illuminate\Database\Seeder;
+
+class TodosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Todo::class, 5)->create();
+    }
+}
